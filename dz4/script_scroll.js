8ffab89e7ac0ccd3417ca89596aa3d1018f5a009
Alina Zhulanova <alina.zhulanava@gmail.com>
2017-10-12 10:45:02 +0300

    norm

--- dz4/script_scroll.js
+++ dz4/script_scroll.js
@@ -1,8 +1,8 @@
 var timeOut;
   function down() {
     if (document.body.scrollBottom!=0 || document.documentElement.scrollBottom!=0){
-    window.scrollBy(0,20);
-    timeOut=setTimeout('down()',10);
+    window.scrollBy(0, 1);
+    timeOut=setTimeout('down()', 30);
     }
   else clearTimeout(timeOut);
 }
