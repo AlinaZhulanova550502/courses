81d32509e4b71b12564445967688229d7ea6c0c5
Alina Zhulanova <alina.zhulanava@gmail.com>
2017-10-30 10:44:52 +0300

    commit

---
+++ dz8/frame.js
@@ -0,0 +1,88 @@
+var F = {
+	width: function(el)
+	{
+		wid = el.scrollWidth;
+		return wid;
+	}
+	height: function(el)
+	{
+		hei = el.scrollHeight;
+		return hei;
+	}
+	pageTop: function(el)
+	{
+		r = el.getBoundingClientRect();
+		top = r.top;
+		return top;
+	}
+	pageLeft: function(el)
+	{
+		r = el.getBoundingClientRect();
+		left = r.left;
+		return left;
+	}
+	css: function(el, prop)
+	{
+		st = el.style[prop];
+		return st;
+	}
+	append: function(e, e2)
+	{
+		e.appendChild(e2);
+	},
+	prepend: function(e, e2, ne)
+	{
+		e.insertBefore(ne, e2,);
+	},
+	remove: function(e)
+	{
+		e.remove();
+	},
+	create: function(tag)
+	{
+		return document.createElement(tag);
+	},
+	Get: {
+			byId : function(id)
+			{
+				return document.getElementById(id);
+			},
+			byClass: function(className)
+			{
+				return document.getElementsByClassName(className);
+			},
+			byTag: function(tag)
+			{
+				return document.getElementsByTagName(tag);
+			},
+			byName: function(name)
+			{
+				return document.getElementsByName(name);
+			},
+			bySelector: function(s)
+			{
+				return document.querySelector(s);
+			},
+			bySelectorAll: function(s)
+			{
+				return document.querySelectorAll(s);
+			},
+		},
+	Event: {
+			add: function(type, elem, f)
+			{
+				if (typeof(elem.addEventListener) == "function") elem.addEventListener(type, f) 
+				else elem.attachEvent(type, f);
+			},
+			remove: function(type, elem, f)
+			{
+				if (typeof(addEventListener) == "function") elem.removeEventListener(type, f)
+				else elem.detachEvent(type, f);
+			},
+			dispatch: function(type, elem)
+			{
+				if (typeof(addEventListener) == "function") elem.dispatchEvent(new Event(type)) 
+				else elem.fireEvent(type, new Event(type));
+			}
+		}
+}
