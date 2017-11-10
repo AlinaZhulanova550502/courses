e28c171794bd58fcaa357a4f7b4c3ad01c9e4cfd
Alina Zhulanova <alina.zhulanava@gmail.com>
2017-11-10 11:13:41 +0300

    commit

--- dz10/point.js
+++ dz10/point.js
@@ -3,12 +3,18 @@ point = new Object();
 Object.defineProperty(point, "x", {
   set: function(value) {
     if (typeof(value) == "number") this.x=value;
+  },
+  get: function() {
+  	return this.x;
   }
 });
 
 Object.defineProperty(point, "y", {
   set: function(value) {
    if (typeof(value) == "number") this.y=value;
+  },
+  get: function(){
+  	return this.y;
   }
 });
 
