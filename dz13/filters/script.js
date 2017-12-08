b150d093b9f91fe628ef611cba7cdcbeba2517c2
Alina Zhulanova <alina.zhulanava@gmail.com>
2017-12-08 11:09:13 +0300

    normal
    norm

--- dz13/filters/script.js
+++ dz13/filters/script.js
@@ -1,23 +1,50 @@
+butOne = document.getElementById("1");
+butTwo = document.getElementById("2");
+butThree = document.getElementById("3");
+
 can = document.getElementById("can");
 var context = can.getContext("2d");
 
 img=document.getElementById("pict");
-
-context.drawImage(img, 0, 0);
+img.onload = function(){
+	context.drawImage(img, 0, 0);
+}
 
 var cb = function(){
-pxCard = context.getImageData(0, 0, 667, 667);
-console.log(pxCard);
-for(i=0; i<pxCard.length; i=i+4)
-{
-	var nc = (pxCard.data[i]+pxCard.data[i+1]+pxCard.data[i+2])/3;
-	pxCard.data[i]=nc;
-	pxCard.data[i+1]=nc;
-	pxCard.data[i+2]=nc;
-}
-console.log(pxCard);
-context.putImageData(pxCard, 0, 0);
+	pxCard = context.getImageData(0, 0, 667, 667);
+	for(i=0; i<pxCard.data.length; i=i+4)
+	{
+		var nc = (pxCard.data[i]+pxCard.data[i+1]+pxCard.data[i+2])/3;
+		pxCard.data[i]=pxCard.data[i+1]=pxCard.data[i+2]=nc;
+	}
+	context.putImageData(pxCard, 0, 0);
+	return 1;
 }
 
-butOne = document.getElementById("1");
+var neg = function(){
+	pxCard = context.getImageData(0, 0, 667, 667);
+	for(i=0; i<pxCard.data.length; i+=4)
+	{
+		pxCard.data[i]=255-pxCard.data[i];
+		pxCard.data[i+1]=255-pxCard.data[i+1];
+		pxCard.data[i+2]=255-pxCard.data[i+2];
+	}
+	context.putImageData(pxCard, 0, 0);
+	return 1;
+}
+
+var my = function(){
+	pxCard = context.getImageData(0, 0, 667, 667);
+	for(i=0; i<pxCard.data.length; i+=4)
+	{
+		pxCard.data[i]=30+pxCard.data[i];
+		pxCard.data[i+1]=20+pxCard.data[i+1];
+		pxCard.data[i+2]=100+pxCard.data[i+2];
+	}
+	context.putImageData(pxCard, 0, 0);
+	return 1;
+}
+
 butOne.addEventListener("click", cb);
+butTwo.addEventListener("click", neg);
+butThree.addEventListener("click", my);
